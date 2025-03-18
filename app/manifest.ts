71ec32dbab97c682e62866ff35b40869bb5a51f4
dimplejethidev <dimplejethidev@gmail.com>
2025-03-18 18:10:41 +0530

    removed logo and name

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,16 +2,16 @@ import type {MetadataRoute} from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "SwipeX",
-    short_name: "SwipeX",
-    description: "Tinder but for betting",
+    name: "Predict Game",
+    short_name: "Predict Game",
+    description: "Predict Game",
     start_url: "/",
     display: "standalone",
     background_color: "#000000",
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon-48x48.png",
+        src: "",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
